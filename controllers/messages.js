50f6cc1ba93a0b2a188226635472fd46e3f8fe17
cataluca <luca.catalano@imakeit.it>
2019-01-14 14:23:25 +0000

    mod title and desc

--- controllers/messages.js
+++ controllers/messages.js
@@ -8,11 +8,11 @@ const render = views(__dirname + '/../views', {
 
 module.exports.home = function *home(ctx) {
   var works = { "work":[
-    {"img":"pav1.jpg","type":"pav","title":"Pavimento in calcestruzzo"},
-    {"img":"pav2.jpg","type":"pav","title":"Pavimento in calcestruzzo"},
-    {"img":"pav3.jpg","type":"pav","title":"Pavimento in calcestruzzo"},
-    {"img":"pav4.jpg","type":"pav","title":"Pavimento in calcestruzzo"},
-    {"img":"pav5.jpg","type":"pav","title":"Pavimento in calcestruzzo"},
+    {"img":"pav1.jpg","type":"pav","title":"Pavimento in cemento armato"},
+    {"img":"pav2.jpg","type":"pav","title":"Pavimento in cemento armato"},
+    {"img":"pav3.jpg","type":"pav","title":"Pavimento in cemento armato"},
+    {"img":"pav4.jpg","type":"pav","title":"Pavimento in cemento armato"},
+    {"img":"pav5.jpg","type":"pav","title":"Pavimento in cemento armato"},
     {"img":"lavato3.jpg","type":"lavato","title":"Pavimento in sasso lavato"},
     {"img":"lavato2.jpg","type":"lavato","title":"Pavimento in sasso lavato"},
     {"img":"lavato1.jpg","type":"lavato","title":"Pavimento in sasso lavato"},
@@ -24,8 +24,8 @@ module.exports.home = function *home(ctx) {
     {"img":"resina2.jpg","type":"resina","title":"Pavimento in resina"},
     {"img":"resina3.jpg","type":"resina","title":"Pavimento in resina"},
     {"img":"resina4.jpg","type":"resina","title":"Pavimento in resina"},
-    {"img":"stampato1.jpg","type":"stampati","title":"Pavimento stampato"},
-    {"img":"stampato2.jpg","type":"stampati","title":"Pavimento stampato"},
+    {"img":"stampato1.jpg","type":"stampati","title":"Pavimento in cemento stampato"},
+    {"img":"stampato2.jpg","type":"stampati","title":"Pavimento in cemento stampato"},
     {"img":"acidificato1.jpg","type":"acidificato","title":"Pavimento acidificato"},
     {"img":"acidificato2.jpg","type":"acidificato","title":"Pavimento acidificato"},
     {"img":"acidificato3.jpg","type":"acidificato","title":"Pavimento acidificato"}
